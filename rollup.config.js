8a505f741ecf8f4475e0b57b21bc7148cd29a52d
Federica Alfano <alfanofederica95@gmail.com>
2018-05-12 21:18:41 +0200

    background manager

--- rollup.config.js
+++ rollup.config.js
@@ -13,7 +13,6 @@ export default {
     sass(),
     url({
       limit: 10 * 1024, // inline files < 10k, copy files > 10k
-      include: ["**/*.svg"], // defaults to .svg, .png, .jpg and .gif files
       emitFiles: true // defaults to true
     }),
     sourcemaps(),
