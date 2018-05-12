c29b521b6875c7e388784276191c8a8ca6d70508
Draktors <enzo.hespel@hotmail.fr>
2018-05-12 16:29:05 +0200

    Fix state manager

--- src/js/managers/state.js
+++ src/js/managers/state.js
@@ -1,10 +1,13 @@
 import DialogManager from './dialog';
 
 class StateManager{
-
   switchToState(state){
     DialogManager.clearChoices();
     DialogManager.clearText();
     state();
   }
 }
+
+const instance = new StateManager();
+
+return instance;
