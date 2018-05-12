4ebf2b6adfaa93cd49d6e743afa5e20b0a24eef6
Federica Alfano <alfanofederica95@gmail.com>
2018-05-12 16:39:13 +0200

    nodes party just starteeed 💃🎉

---
+++ src/nodes/2-haku.js
@@ -0,0 +1,18 @@
+import DialogManager from '../js/managers/dialog';
+import State from '../js/managers/state';
+import nextNode from '../nodes/2-boilerroom';
+
+
+const text1 = 'Salut! Je suis Haku, l\'apprenti de la sorcière Yubaba. Bienvenue dans le monde des esprits! Mais attends, j\'ai l\'impression de t\'avoir déjà rencontrée...';
+const text2 = 'Mmm.. je vois que tu es humaine. Si tu ne veux pas disparaître, tu dois aller chercher du travail.\n' +
+  'Viens, je t\'accompagne à la chaufferie pour rencontrer Kamaji.\n';
+
+function finishText1(){
+  DialogManager.showDialog('Haku', text2, ()=> {State.switchToState(nextNode)} )
+}
+
+
+export default function () {
+  DialogManager.showDialog('Haku', text1, finishText1);
+}
+
