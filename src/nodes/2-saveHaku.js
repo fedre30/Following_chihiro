a312c8b3d2c9268a8417c8ed95aa719c3cd88039
Federica Alfano <alfanofederica95@gmail.com>
2018-05-18 09:33:54 +0200

    video test

--- src/nodes/2-saveHaku.js
+++ src/nodes/2-saveHaku.js
@@ -1,11 +1,11 @@
-import background from '../assets/2-saveHaku.png';
-import background2 from '../assets/hakuHuman.gif';
 import BackgroundManager from '../js/managers/background';
 import song from '../../public/assets/music/haku.mp3';
 import SoundManager from '../js/managers/sound';
 import DialogManager from '../js/managers/dialog';
 import State from '../js/managers/state';
 import nextNode from '../nodes/2-saveNoFace';
+import video from '../assets/haku_human.mov';
+
 
 const text1 = "Oh non! Haku est en danger! \n" +
   "Il a volé le sigille de Zeniba et a été attaqué par des oiseaux en papier.\n" +
@@ -15,12 +15,11 @@ const text2 = "Haku vomit le sigille et reprend sa forme humaine. Chihiro le lai
 
 
 function text1finish() {
-  BackgroundManager.setBackground(background2);
   DialogManager.showDialog('', text2, () => {State.switchToState(nextNode)}, true)
 }
 export default function () {
   SoundManager.clearSound();
   SoundManager.activeSound(song);
-  BackgroundManager.setBackground(background);
+  BackgroundManager.setBackground(video);
   DialogManager.showDialog('', text1, text1finish, true);
 }
